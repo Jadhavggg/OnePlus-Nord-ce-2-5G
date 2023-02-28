9ce6f85cfb3dfa83d915a6522733751cc0e69d16
Ganesh Jadhav <98861204+Jadhavggg@users.noreply.github.com>
2023-02-28 16:57:47 +0530

    Create Android.mk

---
+++ Android.mk
@@ -0,0 +1,21 @@
+############################################
+
+#										   ## Copyright (C) 2021 The LineageOS Project #
+
+#										   #
+
+# SPDX-License-Identifier: Apache-2.0	   #
+
+#										   #
+
+############################################
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),OPIV2201)
+
+$(call add-radio-file,releasetools/dynamic-remove-oplus)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
